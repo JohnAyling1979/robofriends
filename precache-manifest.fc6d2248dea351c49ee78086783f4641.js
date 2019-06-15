8f9d22f81e4d8ba25d380a611b27f99fbe2e2fe9
John Ayling <john.ayling@digintent.com>
2019-06-14 22:22:27 -0500

    Updates

---
+++ precache-manifest.fc6d2248dea351c49ee78086783f4641.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ad3e6203653ee8fd1149f3a478c2aa",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8330e52b09e9e71d45a3",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a9ee426a806ab779b632",
+    "url": "/robofriends/static/css/main.aa870028.chunk.css"
+  },
+  {
+    "revision": "8330e52b09e9e71d45a3",
+    "url": "/robofriends/static/js/2.bd54638b.chunk.js"
+  },
+  {
+    "revision": "a9ee426a806ab779b632",
+    "url": "/robofriends/static/js/main.4e5107ac.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
